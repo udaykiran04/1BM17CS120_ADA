2199435b84729d532d8ec90f38ab837fde0b4490
udaykiran04 <53892004+udaykiran04@users.noreply.github.com>
2019-08-24 10:08:24 +0530

    Add files via upload

---
+++ 2a.bsort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+
+int main()
+{
+	int k,n;
+	cout<<"Enter a number\n";
+	cin>>k;
+	cout<<"Enter the size of array\n";
+	cin>>n;
+	int a[n];
+	cout<<"Enter the array elements:\n";
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	clock_t start,end,t;
+	start=clock();
+	for(int i=0;i<k;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(a[j]<a[j+1])
+			{
+				int temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+				
+		}
+		
+	}
+	
+	end=clock();
+	cout<<"kth smallest element is:\n";
+	cout<<a[n-k];
+	t = end-start;
+	cout<<"\nIt took me: "<<(float)t/CLOCKS_PER_SEC<<" sec\n\n";
+}
---
+++ 2a.ssort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+
+int main()
+{
+	int k,n;
+	cout<<"Enter a number\n";
+	cin>>k;
+	cout<<"Enter the size of array\n";
+	cin>>n;
+	int a[n];
+	cout<<"Enter the array elements:\n";
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	clock_t start,end,t;
+	start=clock();
+	for(int i=0;i<k;i++)
+	{
+		int mpos=i;
+		int min=a[i];
+		for(int j=i+1;j<n;j++)
+		{
+			if(a[j]<min)
+			{
+				min=a[j];
+				mpos=j;
+			}
+				
+		}
+		a[mpos]=a[i];
+		a[i]=min;
+	}
+	end=clock();
+	cout<<"kth smallest element is:\n";
+	cout<<a[k-1];
+	t = end-start;
+	cout<<"\nRuntime is: "<<(float)t/CLOCKS_PER_SEC<<" sec\n\n";
+}
